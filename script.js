5956b02f1fc058c309a7781b12d66e2fab90531a
Anthony <anthgalea1@gmail.com>
2020-12-09 10:06:32 -0500

    added if elsestatements

--- script.js
+++ script.js
@@ -19,10 +19,63 @@ const game = () => {
         const playerHands = document.querySelector('.playerHands');
         const computerHands = document.querySelector('computerHands');
         const allHandSigns = document.querySelectorAll('.handSigns img');
+        //Computer Choices.
+        const computerChoices = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option=>{
+            option.addEventListener('click', function(){
+                //Computer decision from array.
+                const computerNumber = Math.floor(Math.random() *3);
+                const computerDecision = computerChoices[computerNumber];
+                //Call compareTheHandSigns here
+                
+            });
+        });
     };
+
+    const compareTheHandSigns = (playerDecision, computerDecision) =>{
+        //Update text on screen.
+        const theWinner = document.querySelector('.theWinner');
+        //Determine if there is a tie between the player and the computer.
+        if(playerDecision === computerDecision){
+            theWinner.textContent = 'It is a tie';
+            return;
+        }
+        //Determine if player has chosen rock.
+        if(playerDecision === 'rock'){
+            if(computerDecision === 'scissors'){
+                theWinner.textContent = 'Player wins';
+                return;
+            }else{
+                theWinner.textContent = 'Computer wins';
+                return;
+            }
+        }
+        //Determine if player has chosen paper.
+        if(playerDecision === 'paper'){
+            if(computerDecision === 'rock'){
+                theWinner.textContent = 'Player wins';
+                return;
+            }else{
+                theWinner.textContent = 'Computer wins';
+                return;
+            }
+        }
+        //Determine if player has chosen scissors.
+        if(playerDecision === 'scissors'){
+            if(computerDecision === 'paper'){
+                theWinner.textContent = 'Player wins';
+                return;
+            }else{
+                theWinner.textContent = 'Computer wins';
+                return;
+            }
+        }
+    }
 
     //Call all the inner functions. 
     beginGame();
+    playGame();
 };
 
 //Call the game function.
